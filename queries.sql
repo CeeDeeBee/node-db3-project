7719e937d8fe41161be99ae578f435ecd67d67a7
CeeDeeBee <cdbarnes98@gmail.com>
2020-04-15 17:31:57 -0400

    Added scheme helper methods

--- queries.sql
+++ queries.sql
@@ -6,9 +6,9 @@
     JOIN Category ON Product.CategoryId = Category.Id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
-    SELECT o.Id, c.CompanyName, o.OrderDate
+    SELECT o.Id, s.CompanyName
     FROM [Order] as o
-    JOIN Customer as c ON o.CustomerId = c.Id
+    JOIN Shipper as s ON o.ShipVia = s.Id 
     WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
